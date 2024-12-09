b92661946e08be7023de32501e62ccf77ed149d0
Daniel Herszenhut <dhersz@gmail.com>
2024-12-09 17:03:52 -0300

    new function get_cache_dir()

--- R/set_cache_dir.R
+++ R/set_cache_dir.R
@@ -5,7 +5,7 @@
 #'
 #' @param path A string. The path to the directory used to cache the data. If
 #'   `NULL` (the default), the package will use a versioned directory saved
-#'   inside the directory returned by [tools::R_user_dir].
+#'   inside the directory returned by [tools::R_user_dir()].
 #'
 #' @return Invisibly returns the cache directory path.
 #'
@@ -38,4 +38,27 @@ set_cache_dir <- function(path = NULL) {
   writeLines(cache_dir, con = cache_config_file)
 
   return(invisible(cache_dir))
+}
+
+#' Get the cache directory used in geocodebr
+#'
+#' Gets the directory used to cache CNEFE data. Useful to inspect the directory
+#' set with [set_cache_dir()] in a previous R session. Returns the default cache
+#' directory if no custom directory has been previously set.
+#'
+#' @return The cache directory path.
+#'
+#' @examples
+#' get_cache_dir()
+#'
+#' @export
+get_cache_dir <- function() {
+  if (fs::file_exists(cache_config_file)) {
+    cache_dir <- readLines(cache_config_file)
+    cache_dir <- fs::path_norm(cache_dir)
+  } else {
+    cache_dir <- default_cache_dir
+  }
+
+  return(cache_dir)
 }
