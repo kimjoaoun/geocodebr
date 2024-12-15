ee9918dd7e16bd139161a1e85461b2260c919875
rafapereirabr <rafa.pereira.br@gmail.com>
2024-12-14 22:41:25 -0300

    reverse geocode

---
+++ tests/tests_rafa/reverse_geocode.R
@@ -0,0 +1,150 @@
+#' Reverse geocoding
+#'
+#' @description
+#' Reverse geocode coordinate pairs to find the address in CNEFE that most
+#' closely matches the input coordinates.
+#'
+#' @param input_table A data frame. It must contain the columns `'id'`, `'lon'`, `'lat'`
+#' @param ncores Number of cores to be used in parallel execution. Defaults to
+#'        the number of available cores minus 1.
+#' @param progress A logical. Whether to display a download progress bar.
+#'   Defaults to `TRUE`.
+#' @template cache
+#'
+#' @return A `"data.frame"` object.
+#' @export
+#' @family Reverse geocoding
+#' @examplesIf identical(tolower(Sys.getenv("NOT_CRAN")), "true")
+#'
+#' # input data
+#' lonlat_df <- data.frame(
+#'   id = 1:6,
+#'   lon = c(-67.83112, -67.83559, -67.81918, -43.47110, -51.08934, -67.8191),
+#'   lat = c(-9.962392, -9.963436, -9.972736, -22.695578, -30.05981, -9.97273)
+#' )
+#'
+#' # reverse geocode
+#' df_addresses <- geocodebr::reverse_geocode(input_table = lonlat_df)
+#'
+reverse_geocode <- function(input_table,
+                            progress = TRUE,
+                            ncores = NULL,
+                            cache = TRUE
+                            ){
+
+  # check input
+  checkmate::assert_data_frame(input_table)
+  checkmate::assert_logical(progress)
+  checkmate::assert_number(ncores, null.ok = TRUE)
+  checkmate::assert_logical(cache)
+  checkmate::assert_names(
+    names(input_table),
+    must.include = c("id","lon","lat"),
+    .var.name = "input_table"
+  )
+
+  # download cnefe  -------------------------------------------------------
+
+  download_cnefe(state = c("all"), progress = progress)
+
+
+
+  # create db connection -------------------------------------------------------
+  con <- create_geocodebr_db(ncores = ncores)
+
+
+
+  # prep input -------------------------------------------------------
+
+  # create a small range around coordinates
+  data.table::setDT(input_table)
+  margin <- 0.001 # 0.0001
+  input_table[, lon_min := lon - margin]
+  input_table[, lon_max  :=  lon + margin]
+  input_table[, lat_min := lat - margin]
+  input_table[, lat_max  :=  lat + margin]
+
+  # Narrow search scope in cnefe to bounding box
+  bbox_lon_min <- min(input_table$lon_min)
+  bbox_lon_max <- max(input_table$lon_max)
+  bbox_lat_min <- min(input_table$lat_min)
+  bbox_lat_max <- max(input_table$lat_max)
+
+
+  query_filter_cnefe_coords <- glue::glue("
+  CREATE TEMPORARY TABLE filtered_cnefe_coords AS
+  SELECT * FROM cnefe
+  WHERE lon BETWEEN {bbox_lon_min} AND {bbox_lon_max}
+    AND lat BETWEEN {bbox_lat_min} AND {bbox_lat_max}
+  ")
+
+  DBI::dbExecute(con, query_filter_cnefe_coords)
+
+
+
+
+  # comeca cada linha -------------------------------------------------------
+
+  # function to reverse geocode one row of the data
+  reverse_geocode_single_row <- function(df, row_number){
+
+    # row_number = 3
+
+    # subset row
+    temp_df <- input_table[row_number,]
+
+    lon_min <- temp_df$lon_min
+    lon_max <- temp_df$lon_max
+    lat_min <- temp_df$lat_min
+    lat_max <- temp_df$lat_max
+    lon_inp <- temp_df$lon
+    lat_inp <- temp_df$lat
+
+    # Filter cases nearby
+    query_filter_cases_nearby <- glue::glue('
+      SELECT *
+      FROM filtered_cnefe_coords
+      WHERE lon BETWEEN {lon_min} AND {lon_max}
+      AND lat BETWEEN {lat_min} AND {lat_max};
+      ')
+
+    cnefe_nearby <- duckdb::dbSendQuery(con, query_filter_cases_nearby)
+    cnefe_nearby <- duckdb::dbFetch(cnefe_nearby)
+
+    # find closest point
+    data.table::setDT(cnefe_nearby)
+    cnefe_nearby[, lon_diff := abs(lon_inp - lon)]
+    cnefe_nearby[, lat_diff := abs(lat_inp - lat)]
+
+    cnefe_nearest <- cnefe_nearby[, .SD[which.min(lon_diff)]]
+    cnefe_nearest <- cnefe_nearest[, .SD[which.min(lon_diff)]]
+
+    # organize output
+    cnefe_nearest[, c('lon_diff', 'lat_diff') := NULL]
+    temp_df[, c('lon_min', 'lon_max', 'lat_min', 'lat_max') := NULL]
+
+    data.table::setnames(
+      x = temp_df,
+      old = c('lon', 'lat'),
+      new = c('lon_input', 'lat_input')
+    )
+
+    temp_output <- cbind(temp_df, cnefe_nearest)
+
+    return(temp_output)
+  }
+
+
+  output <- pbapply::pblapply(
+    X = 1:nrow(input_table),
+    FUN = reverse_geocode_single_row,
+    df=input_table
+  )
+
+  output <- data.table::rbindlist(output)
+
+  # Disconnect from DuckDB when done
+  duckdb::dbDisconnect(con, shutdown=TRUE)
+
+  return(output)
+}
--- tests/tests_rafa/reverse_geocode_tests.R
+++ tests/tests_rafa/reverse_geocode_tests.R
@@ -6,78 +6,18 @@ library(data.table)
 
 # input -------------------------------------------------------
 
-
-
-lonlat_df <- data.table(id = 1:2,
-                        lon = c(-67.81918, -51.08934, -67.8191),
-                        lat = c(-9.972736, -30.05981, -9.97273)
-                        )
-
-reverse_geocode_arrow(lonlat_df, row_number = 3)
-
-reverse_geocode_arrow <- function(df, row_number){
-
-  # df = lonlat_df
-  # row_number = 1
-
-  # create a small range around coordinates
-  data.table::setDT(df)
-  df[, lon_min := lon - 0.0001]
-  df[, lon_max  :=  lon + 0.0001]
-  df[, lat_min := lat - 0.0001]
-  df[, lat_max  :=  lat + 0.0001]
-
-  temp_lonlat <- df[row_number,]
-
-  lon_min = temp_lonlat$lon_min
-  lon_max = temp_lonlat$lon_max
-  lat_min = temp_lonlat$lat_min
-  lat_max = temp_lonlat$lat_max
-  lon_inp = temp_lonlat$lon
-  lat_inp = temp_lonlat$lat
-
-  # create db connection -------------------------------------------------------
-  db_path <- fs::file_temp(ext = '.duckdb')
-  con <- duckdb::dbConnect(duckdb::duckdb(), dbdir=db_path)
-
-  # Load CNEFE data and write to DuckDB
-  cache_dir <- 'C:/Users/user/Downloads/geocodebr_cache'
-  cnefe <- arrow::open_dataset(cache_dir) |> collect()
-  # cnefe <- arrow_open_dataset(geocodebr_env$cache_dir)
-  # duckdb::duckdb_register_arrow(con, "cnefe", cnefe)
-  duckdb::dbWriteTable(con, "cnefe", cnefe,
-                       temporary = TRUE, overwrite=TRUE
-                       )
-
-  duckdb::dbExistsTable(con, 'cnefe')
+lonlat_df <- data.table(
+  id = 1:6,
+  lon = c(-67.83112, -67.83559, -67.81918, -43.47110, -51.08934, -67.8191),
+  lat = c(-9.962392, -9.963436, -9.972736, -22.695578, -30.05981, -9.97273)
+  )
 
 
 
 
-  temp_cnefe <- filter(cnefe, dplyr::between(lon, lon_min, lon_max)) |>
-                filter(dplyr::between(lat, lat_min, lat_max)) |>
-                mutate(lon_diff = lon_inp - lon,
-                       lat_diff =  lat_inp - lat) |>
-                collect()
+
+a <- reverse_geocode(lonlat_df)
 
 
-  temp_output <- temp_cnefe |>
-          filter(lon_diff == min(lon_diff)) |>
-          filter(lat_diff == min(lat_diff)) |>
-          select(-lon_diff , -lat_diff)
-
-
-  return(temp_output)
-
-}
-
-# query_filter_lonlat_range <- glue::glue('
-#   SELECT *
-#   FROM cnefe
-#   WHERE lon BETWEEN {lon_min} AND {lon_max}
-#   AND lat BETWEEN {lat_min} AND {lat_max};
-#   ')
-#
-# result <- DBI::dbExecute(con, query_filter_lonlat_range)
-
-
+ttt <- data.frame(id=1, lat=-15.814192047159876, lon=-47.90534614672923)
+reverse_geocode(df = ttt)
