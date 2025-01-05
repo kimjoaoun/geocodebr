eade0546ab6aac4975bd6fea9a765c176cee936e
rafapereirabr <rafa.pereira.br@gmail.com>
2025-01-05 10:38:13 -0300

    local with arrow architecture

--- R/geocode_rafa_local.R
+++ R/geocode_rafa_local.R
@@ -105,7 +105,13 @@ geocode_rafa_local <- function(addresses_table,
   input_padrao[, numero := as.integer(numero)]
 
   # creating a temporary db and register the input table data
-  con <- duckdb::dbConnect(duckdb::duckdb(), dbdir = 'geocodebr_db1.duckdb')
+  con <- duckdb::dbConnect(
+    duckdb::duckdb(),
+    dbdir = 'C:/Users/r1701707/AppData/Local/R/cache/R/geocodebr/test_db/geocodebr_db1.duckdb'
+  #  dbdir = 'C:/Users/r1701707/AppData/Local/R/cache/R/geocodebr/test_db/geo.duckdb'
+  )
+
+
 
   # Set Number of cores for parallel operation
   if (is.null(n_cores)) {
@@ -215,7 +221,7 @@ geocode_rafa_local <- function(addresses_table,
   all_output_tbs <- output_tables[!grepl('empty', output_tables)]
 
   # save output to db
-  output_query <- paste("CREATE OR REPLACE VIEW output_db AS",
+  output_query <- paste("CREATE OR REPLACE TEMPORARY VIEW output_db AS",
                         paste0("SELECT ", paste0('*', " FROM ", all_output_tbs),
                                collapse = " UNION ALL ")
   )
@@ -355,7 +361,11 @@ geocode_rafa_local2 <- function(addresses_table,
   input_padrao[, numero := as.numeric(numero)]
 
   # creating a temporary db and register the input table data
-  con <- duckdb::dbConnect(duckdb::duckdb(), dbdir = 'geocodebr_db.duckdb')
+  con <- duckdb::dbConnect(
+    duckdb::duckdb(),
+    dbdir = 'C:/Users/r1701707/AppData/Local/R/cache/R/geocodebr/test_db/geocodebr_db.duckdb'
+    #  dbdir = 'C:/Users/r1701707/AppData/Local/R/cache/R/geocodebr/test_db/geo.duckdb'
+  )
 
   # Set Number of cores for parallel operation
   if (is.null(n_cores)) {
@@ -453,7 +463,236 @@ geocode_rafa_local2 <- function(addresses_table,
   all_output_tbs <- output_tables[!grepl('empty', output_tables)]
 
   # save output to db
-  output_query <- paste("CREATE OR REPLACE VIEW output_db AS",
+  output_query <- paste("CREATE OR REPLACE TEMPORARY VIEW output_db AS",
+                        paste0("SELECT ", paste0('*', " FROM ", all_output_tbs),
+                               collapse = " UNION ALL ")
+  )
+
+
+  DBI::dbExecute(con, output_query)
+
+
+  # output with all original columns
+  duckdb::dbWriteTable(con, "input_padrao_db", input_padrao,
+                       temporary = TRUE, overwrite=TRUE)
+
+  x_columns <- names(input_padrao)
+
+  output_deterministic <- merge_results(
+    con,
+    x='input_padrao_db',
+    y='output_db',
+    key_column='id',
+    select_columns = x_columns
+  )
+
+  # Disconnect from DuckDB when done
+  duckdb::dbDisconnect(con, shutdown=TRUE)
+
+  # Return the result
+  return(output_deterministic)
+}
+
+
+
+
+
+#' Geocoding addresses based on CNEFE data
+#'
+#' @description
+#' Takes a data frame containing addresses as an input and returns the spatial
+#' coordinates found based on CNEFE data.
+#'
+#' @param addresses_table A data frame. The addresses to be geocoded. Each
+#'   column must represent an address field.
+#' @param address_fields A character vector. The correspondence between each
+#'   address field and the name of the column that describes it in
+#'   `addresses_table`. The [setup_address_fields()] function helps creating
+#'   this vector and performs some checks on the input. Address fields
+#'   passed as `NULL` are ignored and the function must receive at least one
+#'   non-null field. If manually creating the vector, please note that the
+#'   vector names should be the same names used in the [setup_address_fields()]
+#'   parameters.
+#' @template n_cores
+#' @template progress
+#' @template cache
+
+#'
+#' @return An arrow `Dataset` or a `"data.frame"` object.
+#'
+#' @examplesIf identical(tolower(Sys.getenv("NOT_CRAN")), "true")
+#'
+#' data_path <- system.file("extdata/small_sample.csv", package = "geocodebr")
+#' input_df <- read.csv(data_path)
+#'
+#' fields <- setup_address_fields(
+#'   logradouro = "nm_logradouro",
+#'   numero = "Numero",
+#'   cep = "Cep",
+#'   bairro = "Bairro",
+#'   municipio = "nm_municipio",
+#'   estado = "nm_uf"
+#' )
+#'
+#' #df <- geocodebr:::geocode_rafa_local(input_df, address_fields = fields, progress = FALSE)
+#' #df
+#'
+geocode_rafa_local_arrow <- function(addresses_table,
+                                address_fields = setup_address_fields(),
+                                n_cores = 1,
+                                progress = TRUE,
+                                cache = TRUE
+){
+
+  # check input
+  assert_address_fields(address_fields, addresses_table)
+  checkmate::assert_data_frame(addresses_table)
+  checkmate::assert_number(n_cores, lower = 1, finite = TRUE)
+  checkmate::assert_logical(progress, any.missing = FALSE, len = 1)
+  checkmate::assert_logical(cache, any.missing = FALSE, len = 1)
+  checkmate::assert_names(
+    names(addresses_table),
+    must.include = "id"
+  )
+
+
+  # normalize input data -------------------------------------------------------
+
+  # standardizing the addresses table to increase the chances of finding a match
+  # in the CNEFE data
+
+  if (progress) message_standardizing_addresses()
+
+  input_padrao <- enderecobr::padronizar_enderecos(
+    addresses_table,
+    campos_do_endereco = enderecobr::correspondencia_campos(
+      logradouro = address_fields[["logradouro"]],
+      numero = address_fields[["numero"]],
+      cep = address_fields[["cep"]],
+      bairro = address_fields[["bairro"]],
+      municipio = address_fields[["municipio"]],
+      estado = address_fields[["estado"]]
+    ),
+    formato_estados = "sigla"
+  )
+
+
+  # keep and rename colunms of input_padrao to use the
+  # same column names used in cnefe data set
+  data.table::setDT(input_padrao)
+  cols_padr <- grep("_padr", names(input_padrao), value = TRUE)
+  input_padrao <- input_padrao[, .SD, .SDcols = c("id", cols_padr)]
+  names(input_padrao) <- c("id", gsub("_padr", "", cols_padr))
+
+  data.table::setnames(
+    x = input_padrao,
+    old = c('logradouro', 'bairro'),
+    new = c('logradouro_sem_numero', 'localidade'))
+
+
+
+  # downloading cnefe. we only need to download the states present in the
+  # addresses table, which may save us some time.
+
+  # cnefe_dir <- download_cnefe(
+  #   input_states,
+  #   progress = progress,
+  #   cache = cache
+  # )
+
+  ### temporary
+  input_padrao[, numero := as.numeric(numero)]
+
+  # creating a temporary db and register the input table data
+  con <- create_geocodebr_db(n_cores = n_cores)
+
+  # Convert input data frame to DuckDB table
+  duckdb::dbWriteTable(con, "input_padrao_db", input_padrao, temporary = TRUE)
+
+
+
+
+  # START DETERMINISTIC MATCHING -----------------------------------------------
+
+  # when merging the data, we have several different cases with different confidence
+  # levels. from best to worst, they are:
+
+  # - case 01: match municipio, logradouro, numero, cep, localidade
+  # - case 02: match municipio, logradouro, numero, cep
+  # - case 03: match municipio, logradouro, numero, localidade
+  # - case 04: match municipio, logradouro, numero
+  # - case 44: match municipio, logradouro, numero (interpolado)
+  # - case 05: match municipio, logradouro, cep, localidade
+  # - case 06: match municipio, logradouro, cep
+  # - case 07: match municipio, logradouro, localidade
+  # - case 08: match municipio, logradouro
+  # - case 09: match municipio, cep, localidade
+  # - case 10: match municipio, cep
+  # - case 11: match municipio, localidade
+  # - case 12: match municipio
+
+  if (progress) {
+    prog <- create_progress_bar(input_padrao)
+    n_rows_affected <- 0
+
+    message_looking_for_matches()
+  }
+
+  # determine geographical scope of the search
+  input_states <- unique(input_padrao$estado)
+  input_municipio <- unique(input_padrao$municipio)
+
+  input_municipio <- input_municipio[!is.na(input_municipio)]
+  if(is.null(input_municipio)){ input_municipio <- "*"}
+
+
+  # (case in c(1:4, 44, 5:12))
+  for (case in c(1:4,  5:12)) {
+
+    relevant_cols <- get_relevant_cols_rafa(case)
+    formatted_case <- formatC(case, width = 2, flag = "0")
+
+    if (progress) update_progress_bar(n_rows_affected, formatted_case)
+
+
+    if (all(relevant_cols %in% names(input_padrao))) {
+
+      # select match function
+      match_fun <- ifelse(case != 44, match_aggregated_cases_local_arrow, match_aggregated_cases_weighted)
+
+      n_rows_affected <- match_fun(
+        con,
+        x = 'input_padrao_db',
+        y = 'filtered_cnefe', # desnecessario
+        output_tb = paste0('output_caso_', formatted_case),
+        key_cols = relevant_cols,
+        match_type = case,
+        input_states = input_states,
+        input_municipio = input_municipio
+      )
+    }
+
+  }
+
+  if (progress) finish_progress_bar(n_rows_affected)
+
+
+  # prepare output -----------------------------------------------
+  # THIS could BE IMPROVED / optimized
+
+  # list all table outputs
+  all_possible_tables <- glue::glue("output_caso_{formatC(1:12, width = 2, flag = '0')}")
+
+  # check which tables have been created
+  output_tables <- lapply(
+    X= all_possible_tables,
+    FUN = function(i){ ifelse( DBI::dbExistsTable(con, i), i, 'empty') }) |>
+    unlist()
+
+  all_output_tbs <- output_tables[!grepl('empty', output_tables)]
+
+  # save output to db
+  output_query <- paste("CREATE OR REPLACE TEMPORARY VIEW output_db AS",
                         paste0("SELECT ", paste0('*', " FROM ", all_output_tbs),
                                collapse = " UNION ALL ")
   )
