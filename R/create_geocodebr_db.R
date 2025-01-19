cb7b03fbf77e082053f3df50b4b2376aef3cd493
rafapereirabr <rafa.pereira.br@gmail.com>
2025-01-18 21:13:33 -0300

    removing fs

--- R/create_geocodebr_db.R
+++ R/create_geocodebr_db.R
@@ -2,9 +2,10 @@
 #'
 #' Builds a duckdb connection with the CNEFE data set in the cache direcotry.
 #'
-#' @param db_path A character vector pointing to an existing directory where a
-#'        a persistent '.duckdb' file is created. Defaults to a temporary
-#'        directory `tempdir()`.
+#' @param db_path Character. If Defaults `db_path = "tempdir"` (the default),
+#'        the duckdb connection is created on temporary directory. If
+#'        `db_path = "memory"`, the duckdb connection is created on the RAM
+#'        memory.
 #' @template n_cores
 #'
 #' @return A duckdb connection.
@@ -13,22 +14,27 @@
 #'
 #' @keywords internal
 #'
-create_geocodebr_db <- function(db_path = tempdir(),
+create_geocodebr_db <- function(db_path = "tempdir",
                                 n_cores = NULL
                                 ){
 
-
-
   # check input
   checkmate::assert_number(n_cores, null.ok = TRUE)
+  # checkmate::assert_string(db_path, pattern = "tempdir|memory")
 
 
-  # create db connection
   # this creates a local database which allows DuckDB to
   # perform **larger-than-memory** workloads
-  db_path <- fs::file_temp(tmp_dir = db_path, ext = '.duckdb')
-  con <- duckdb::dbConnect(duckdb::duckdb(), dbdir= db_path ) # db_path ":memory:"
+  if(db_path == 'tempdir'){
+    db_path <- tempfile(pattern = 'geocodebr', fileext = '.duckdb')
+  }
 
+  if(db_path == 'memory'){
+    con <- duckdb::dbConnect(duckdb::duckdb(), dbdir= ":memory:" )
+  }
+
+  # create db connection
+  con <- duckdb::dbConnect(duckdb::duckdb(), dbdir= db_path ) # db_path ":memory:"
 
   # Set Number of cores for parallel operation
   if (is.null(n_cores)) {
@@ -39,9 +45,8 @@ create_geocodebr_db <- function(db_path = tempdir(),
 
   DBI::dbExecute(con, sprintf("SET threads = %s;", n_cores))
 
-  # test
   # Set Memory limit
   # DBI::dbExecute(con, "SET memory_limit = '8GB'")
 
   return(con)
-  }
+}
