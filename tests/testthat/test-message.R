34b09eb5ca9c74d3e7542c435136ee699ad11948
rafapereirabr <rafa.pereira.br@gmail.com>
2025-03-09 22:43:45 -0300

    vignettes and docs

--- tests/testthat/test-message.R
+++ tests/testthat/test-message.R
@@ -7,4 +7,22 @@ test_that("message_works_correctly", {
   expect_message(parent_function(), class = "geocodebr_message")
 
   expect_snapshot(parent_function(), cnd_class = TRUE)
+
+  expect_message(message_standardizing_addresses())
+  expect_message(message_baixando_cnefe())
+  expect_message(message_looking_for_matches())
+  expect_message(message_preparando_output())
 })
+
+
+# error
+test_that("message_does_NOT_work_correctly", {
+  expect_error(message_test("banana"))
+})
+
+
+
+
+
+
+
